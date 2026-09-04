9f325bc7b38770471ec32309d17e2b4455af0b6e
marcio rodriguez <marcinhoperez10@gmail.com>
2026-09-04 12:57:29 -0300

    implementacao static line

--- mrpf/parser.c
+++ mrpf/parser.c
@@ -24,3 +24,18 @@ static int parse_positive_int(const char *token, int *out){
     *out = (int) value;
     return 1;
 }
+
+static void trim_line(char *line){
+    size_t len = strlen(line);
+    while (len > 0 && (line[len - 1] == '\n' || line[len - 1] == '\r' ||
+                        isspace((unsigned char) line[len - 1]))) {
+        line[--len] = '\0';
+    }
+    size_t start = 0;
+    while (line[start] != '\0' && isspace((unsigned char) line[start])) {
+        start++;
+    }
+    if (start > 0) {
+        memmove(line, line + start, len - start + 1);
+    }
+}
